63613db345cf72b3b0ae52c4572a8d90e9172edd
JhoanVanDam2 <jhoanesneider56@gmail.com>
2023-05-23 21:31:08 -0500

    Se ágregó el modulo forgot 3 repair

--- src/Components/OlvideContraseña/forgot.jsx
+++ src/Components/OlvideContraseña/forgot.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Forgotstyle from './forgot.css';
 import bee from "../../Resources/abeja.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Navbar from '../Navbar/navbar';
 import Footer from '../Footer/footer';
 class Forgot extends Component {
